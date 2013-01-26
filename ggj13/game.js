8a89ff668969beabbf5ae97663a6580362e5af53
Alex <nemokervi@yahoo.fr>
2013-01-26 08:37:26 +0100

    Ajout des Globule Blanc Fonctionnel

--- ggj13/game.js
+++ ggj13/game.js
@@ -1,11 +1,11 @@
 // Variables
-  //Système
-    var width = 1024,  
-    //width of the canvas  
+  //SystÃ¨me
+    var width = 1024,
+    //width of the canvas
       height = 600,
     //height of the canvas
       c = document.getElementById('c'),
-    //canvas itself   
+    //canvas itself
       ctx = c.getContext('2d');
       
       var showDebug = false;
@@ -16,6 +16,7 @@
       var g_maxVSpeed = 100;
       var g_thrustForce = -5;
       var g_gravity = 3;
+      var g_obstacleInterval = 500;
       //END
       
       //VARIABLES GAMEPLAY
@@ -28,9 +29,9 @@
       var currentState = 0;
       
       var ballRunning = false;
-    //and two-dimensional graphic context of the  
-    //canvas, the only one supported by all   
-    //browsers for now  
+    //and two-dimensional graphic context of the
+    //canvas, the only one supported by all
+    //browsers for now
       
     c.width = width;
     c.height = height;
@@ -67,30 +68,36 @@ function virusClass(options)
   }
 }
 
-//function 
+//function
 
 function globuleClass(options)
 {
   this.x = options.x;
   this.y = options.y;
   this.width = 64;
+  this.height = 32;
+}
+
+function AjoutGlobule(){
+  //obstacles = new Array();
+  obstacles.push(new globuleClass({x:950, y:Math.round((Math.random()*1200)/2)}));
 }
 
 var clear = function()
-{  
-  ctx.fillStyle = '#800000';  
+{
+  ctx.fillStyle = '#800000';
 
-//clear whole surface  
-  ctx.beginPath();  
+//clear whole surface
+  ctx.beginPath();
 //start drawing
-  ctx.rect(0, 0, width, height);  
-//draw rectangle from point (0, 0) to  
-//(width, height) covering whole canvas  
+  ctx.rect(0, 0, width, height);
+//draw rectangle from point (0, 0) to
+//(width, height) covering whole canvas
   ctx.closePath();
-//end drawing  
+//end drawing
   ctx.fill();
-//fill rectangle with active  
-//color selected before  
+//fill rectangle with active
+//color selected before
 }
 
 function GetGeneralSpeed()
@@ -102,6 +109,7 @@ function InitElts()
 {
   player = new virusClass({x:32, y:height/2});
   obstacles = new Array();
+  AjoutGlobule();
 }
 
 function DistFrom(originX, originY, targetX, targetY)
@@ -134,7 +142,7 @@ $("#c").mousedown(function()
 $("#c").mouseup(function()
 {
   if(isMouseDown)
-  { 
+  {
     isMouseDown = false;
   }
 });
@@ -165,97 +173,106 @@ function UpdateElements()
     player.y = height - player.height;
     player.verticalVelocity = 0;
   }
+
+  
+  for(var i = 0; i<obstacles.length; i++){
+    obstacles[i].x-=GetGeneralSpeed();
+  }
+  if((width-g_obstacleInterval)>obstacles[obstacles.length-1].x){
+      obstacles.push(new globuleClass({x:950, y:Math.round((Math.random()*1200)/2)}));
+    }
 }
 
 function DrawAll()
 {
   ctx.fillStyle = "#080";
   ctx.fillRect(player.x, player.y, player.width, player.height);
-/*  var i=0;
-  var destRotatedx;
-  var destRotatedy;
-  for(i=0;i<elements.length;i++)
-  {
-    var centerx = elements[i].x;
-    var centery = elements[i].y;
-    ctx.fillStyle = GetColorFromIndex(i);
-    if(i == selectedElement)
-      ctx.strokeStyle = "#FFF"
-    else
-      ctx.strokeStyle = "#000";
-    ctx.beginPath();
-    ctx.arc(elements[i].x,elements[i].y,elements[i].width,0,2*Math.PI);
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
-    
-    
-    destRotatedx = (elements[i].width + 10) * Math.cos(elements[i].direction);
-    destRotatedy = (elements[i].width + 10) * Math.sin(elements[i].direction);
-    
-    ctx.fillStyle = GetColorFromIndex(i+1);
-    ctx.beginPath();
-    ctx.arc(centerx + destRotatedx, centery+destRotatedy, 8, 0, 2*Math.PI);
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
-    
-    if(i == selectedElement)
-    {
-      ctx.beginPath();
-      ctx.lineWidth = 1;
-      ctx.moveTo(centerx, centery)
-      ctx.lineTo(centerx + gapx, centery + gapy);
-      //ctx.lineTo(centerx + gapx, centery);
-      //ctx.lineTo(centerx, centery);
-      ctx.stroke();
-      ctx.closePath();
-    }
-  }
-  
-  for(i=0; i<animations.length; i++)
-  {
-    animations[i].Draw(ctx);
-  }
-  
-  if(musicRunning)
-  {
-    ctx.fillStyle = GetColorFromIndex(ball.currentRebound+1);
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.width, 0, 2*Math.PI);
-    ctx.fill();
-    ctx.closePath();
-    
-    var i = 0;
-    while(i < ball.currentRebound)
-    {
-      var nextIndex = (i+1) % elements.length;
-      ctx.strokeStyle = GetColorFromIndex(i);
-      ctx.beginPath();
-      ctx.moveTo(elements[i].x, elements[i].y);
-      ctx.lineTo(elements[nextIndex].x, elements[nextIndex].y);
-      ctx.stroke();
-      ctx.closePath();
-      i++;
-    }
-    ctx.strokeStyle = GetColorFromIndex(ball.currentRebound);
-    ctx.beginPath();
-    ctx.moveTo(elements[ball.currentRebound].x, elements[ball.currentRebound].y);
-    ctx.lineTo(ball.x, ball.y);
-    ctx.stroke();
-    ctx.closePath();
-    
-  }
-  
-  ctx.fillStyle = "#000";
-  ctx.strokeStyle = "#FFF";
-  ctx.fillRect(btnStartX, btnStartY, btnStartWidth, btnStartHeight);
-  ctx.strokeRect(btnStartX, btnStartY, btnStartWidth, btnStartHeight);
-  ctx.font = "12px sans-serif";
+
   ctx.fillStyle = "#FFF";
-  ctx.textAlign = "center"
-  ctx.fillText("START", btnStartX + btnStartWidth/2, btnStartY + btnStartHeight/2);
-  //ctx.fillText(destRotatedx + " " + destRotatedy, 550, 350);*/
+  for(var i = 0; i<obstacles.length; i++){
+    ctx.beginPath();
+    ctx.arc(obstacles[i].x, obstacles[i].y, 30, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  //ctx.fillRect(ennemis.x, ennemis.y, ennemis.width, ennemis.height);
+/* var i=0;
+var destRotatedx;
+var destRotatedy;
+for(i=0;i<elements.length;i++)
+{
+var centerx = elements[i].x;
+var centery = elements[i].y;
+ctx.fillStyle = GetColorFromIndex(i);
+if(i == selectedElement)
+ctx.strokeStyle = "#FFF"
+else
+ctx.strokeStyle = "#000";
+ctx.beginPath();
+ctx.arc(elements[i].x,elements[i].y,elements[i].width,0,2*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+destRotatedx = (elements[i].width + 10) * Math.cos(elements[i].direction);
+destRotatedy = (elements[i].width + 10) * Math.sin(elements[i].direction);
+ctx.fillStyle = GetColorFromIndex(i+1);
+ctx.beginPath();
+ctx.arc(centerx + destRotatedx, centery+destRotatedy, 8, 0, 2*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+if(i == selectedElement)
+{
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.moveTo(centerx, centery)
+ctx.lineTo(centerx + gapx, centery + gapy);
+//ctx.lineTo(centerx + gapx, centery);
+//ctx.lineTo(centerx, centery);
+ctx.stroke();
+ctx.closePath();
+}
+}
+for(i=0; i<animations.length; i++)
+{
+animations[i].Draw(ctx);
+}
+if(musicRunning)
+{
+ctx.fillStyle = GetColorFromIndex(ball.currentRebound+1);
+ctx.beginPath();
+ctx.arc(ball.x, ball.y, ball.width, 0, 2*Math.PI);
+ctx.fill();
+ctx.closePath();
+var i = 0;
+while(i < ball.currentRebound)
+{
+var nextIndex = (i+1) % elements.length;
+ctx.strokeStyle = GetColorFromIndex(i);
+ctx.beginPath();
+ctx.moveTo(elements[i].x, elements[i].y);
+ctx.lineTo(elements[nextIndex].x, elements[nextIndex].y);
+ctx.stroke();
+ctx.closePath();
+i++;
+}
+ctx.strokeStyle = GetColorFromIndex(ball.currentRebound);
+ctx.beginPath();
+ctx.moveTo(elements[ball.currentRebound].x, elements[ball.currentRebound].y);
+ctx.lineTo(ball.x, ball.y);
+ctx.stroke();
+ctx.closePath();
+}
+ctx.fillStyle = "#000";
+ctx.strokeStyle = "#FFF";
+ctx.fillRect(btnStartX, btnStartY, btnStartWidth, btnStartHeight);
+ctx.strokeRect(btnStartX, btnStartY, btnStartWidth, btnStartHeight);
+ctx.font = "12px sans-serif";
+ctx.fillStyle = "#FFF";
+ctx.textAlign = "center"
+ctx.fillText("START", btnStartX + btnStartWidth/2, btnStartY + btnStartHeight/2);
+//ctx.fillText(destRotatedx + " " + destRotatedy, 550, 350);*/
 }
 
 function DrawIntro()
@@ -283,8 +300,7 @@ var GameLoop = function()
       UpdateElements();
       DrawAll();
   }
-  
-  gLoop = setTimeout(GameLoop, 1000 / 50);  
+  gLoop = setTimeout(GameLoop, 1000 / 50);
 }
 InitElts();
 
